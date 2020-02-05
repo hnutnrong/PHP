b93019a9abea31115004944ee12fe4d736476280
Nutnarong <35445939+hnutnrong@users.noreply.github.com>
2020-02-05 11:19:54 +0700

    [insert]ViewRegister
    สร้าง view หน้า register

---
+++ basicphp/formregister/register.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title></title>
+</head>
+<body>
+<h1>ลงทะเบียน</h1>
+    <form action="register.php" method="POST">
+    <label for ="username">Username: </label>
+    <input type ="text" name ="username" require autofocus>
+    <label for ="email">E-mail: </label>
+    <input type ="email" name ="email" placeholder="example@gmail.com">
+    <br><br>
+
+    
+    <input type="submit" value="ลงทะเบียน">
+</body>
+</html>
